a4c46f657e8ad4e79890557c50a41c5753fe8864
Santiago <santiago2004gjq@gmail.com>
2024-10-02 15:24:30 -0500

    Actualizacion Login
    Se crea pagina login

--- src/head.php
+++ src/head.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/principal.css">
     <link rel="stylesheet" href="css/blog.css">
+    <link rel="stylesheet" href="css/index.css">
+
     <link rel="stylesheet" href="css/articulos.css"> <!--Articulos o Publiidad-->
     
     <script src="js/script.js"></script>
