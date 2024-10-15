5a019cec10a9bb41680bffd3ad871cccc73d478a
Santiago <santiago2004gjq@gmail.com>
2024-10-15 14:44:07 -0500

    Cargar Pagina Nueva
    Cargar Pagina Nueva

--- index.php
+++ index.php
@@ -1,72 +1,73 @@
 <!DOCTYPE html>
-<html lang="es">
-<?php include 'src/head.php'; ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejemplo</title>
+    <link rel="stylesheet" href="css/pagina_web.css">
+</head>
 <body>
-    <!--Cabezera-->
-    <header>
-        <div class="header-content">
-            <div class="logo">
-                <h1><b>S-S</b> Financiacion Familiar</h1>
-            </div>
-            <!--Menu-->
-            <div class="menu" id="show-menu">
-                <nav>
-                    <ul>
-                        <li class="menu-selected"><a href="#"  class="text-menu-selected"> <i class="fas fa-home"></i>Inicio</a></li>
-                        <li><a href="#"> <i class="fab fa-file"> </i>Ingresos</a></li>
-                        <li><a href="blog.html"><i class="fas fa-file-alt"></i>Gastos</a></li>
-                        <li><a href="#"> <i class="fas fa-headset"></i>Presupuesto</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-        <div id="icon-menu">
-            <i class="fa-solid fa-bars"></i>
-        </div>
-    </header>
-
-    <div class="container-all" id="move-content">
-
-    <!--Portada-->
-
-    <div class="blog-container-cover">
-        <div class="container-info-cover">
-            <h1>¡Todo es posible!</h1>
-            <p>---s---</p>
-        </div>
-    </div>
-
-    <!--Contenido-->
-
-    <div class="container-content">
-        <article><br>
-            <h1>Contenido</h1>
-            <p>Hola Mundo</p>
-            <ul>
-                <li>Inicio</li>
-                <ul>
-                    <li>hola</li>
-                </ul>
-                <li>Tema 1</li>
-            </ul>
+    <div >
+        <header>
+            <img src="imagenes/paginam.jpeg" width="300">
             
-            <br><img src="img/imagen.png" alt="">
-        </article>
-
-        <div class="container-aside">	
-            <aside>
-                <img src="img/img6.jpg" alt="">
-                <h2>Titulo del articulo</h2>
-                <p>Espacio de Publiidad-- o Articulo!</p>
-                <a href="#"><button>leer más</button></a>
-            </aside>
-        </div>
+            <h1>Mi Sitio</h1>
+        
+        
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+        </header>
     </div>
+    <section class="main">
+        <section class="articulos">
+            <article>
+                <h1>Título de mi sitio web</h1>
+        <p>
+            Una página web es un documento estructurado. El elemento central de una página web es un archivo de texto escrito en el Lenguaje de Marcado de Hipertexto (HTML) que especifica el contenido de la página web (incluidos los enlaces llamados hipervínculos) a otros recursos web, principalmente otras páginas web, y a diferentes secciones de la misma página web.
+        
+        
+            Un documento HTML puede incluir archivos independientes llamados hojas de estilo en cascada (CSS) (que también son archivos de texto) que especifican la presentación del contenido en una página web.
+            Se trata, en principio, de una alternativa a incluir esa información directamente en el código HTML.
+        
+        
+            El documento también puede incluir programas JavaScript o WebAssembly, que son ejecutados por el navegador web para añadir un comportamiento dinámico a la página web.
+        </p>
+            </article>
+            <article>
+                <h2>Etiquetas</h2>
+        <p>
+            Por ejemplo, puedes utilizar etiquetas HTML para aplicar cursivas, crear saltos de línea, insertar objetos multimedia, crear listas con viñetas o para definir diferentes tipos de contenido en una página web, como encabezados, párrafos, imágenes, enlaces, formularios y mucho más.
+            Los servidores leen el código HTML para entender y mostrar el contenido.
+        </p>
+            </article>
+        </section>
+        <aside>
+            <h3> aside</h3>
+          <p>
+            Un texto es una composición de signos codificados en un sistema de escritura que forma una unidad de sentido. También es una composición de caracteres imprimibles generados por un algoritmo de cifrado que, aunque no tienen sentido para cualquier persona, sí puede ser descifrado por su destinatario original 
+          </p>
+        </aside>
+    </section>
+    <footer>
+    <div class="container-footer">	
+    <footer>
+        <div class="logo-footer">
+            <img src="img/imagen.png" alt="">
+        </div>
 
-    <!--Pie de pagina-->
-    <?php include 'src/footer.php'; ?>
-</div>
+        <hr>
+        <p class="fecha"></p>
+        <p class="tiempo"></p>
+        <h4>© 2024 - Titulo - Todos los Derechos Reservados</h4>
+        <p>© 2024 Paula Gómez</p>
+        </div>
+    </footer>
 </body>
-<script src="js/script.js"></script>
 <script src="js/reloj.js"></script>
 </html>
